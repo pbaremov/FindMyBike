5e9c2c601e96132825fb39f37ba1f6d3f04d7017
petko <baremov.petko@gmail.com>
2024-07-01 12:43:14 +0100

    start search button is added

--- execute_search.py
+++ execute_search.py
@@ -1,46 +1,6 @@
-from getbikes import bikes_list, get_bikes, get_images_titles, front_tags
-
-# from robo_detector import robo_detector
-
-
-import streamlit as st
-from streamlit_image_select import image_select
-from streamlit_carousel import carousel
-
-
-st.set_page_config(layout="wide", page_title="FindMyBike WebApp", page_icon=None)
-
-# title
-st.title("Find your bicycle on pinkbike.com")
-
-st.write("[https://roboflow.com/](https://roboflow.com/)")
-# header
-st.header("Please provide details of your RoboFlow detector model")
-
-# upload your detector                //possibly 3 input fields 1. Api key 2. project name 3 version
-col1, col2, col3 = st.columns(3)
-
-api_key = col1.text_input("Provide your API key")
-project_name = col2.text_input("Provide your project name")
-project_version = col3.text_input("Provide your project version")
-
-
-# save your detector // this may be optional
-
-
-# load the detector
-
-# print(api_key)
-
-# upload your images
-# photo = st.file_uploader(type=["png, jpeg, jpg"], label="upload your file")
-
-
-# include some sort of a progress bar
-
-
 # run the code that will scrape through all the HTML and will get the urls of all the images
-
+import streamlit as st 
+from getbikes import bikes_list, get_bikes, get_images_titles, front_tags
 
 def execute_search():
     i = 1
@@ -63,7 +23,6 @@ def execute_search():
             }
             # print(f"bike number {n}")
             n += 1
-
             # print(f'Len(bike[images]) is {len(bike["images"])}')
 
             # print(bike)
@@ -101,10 +60,3 @@ def execute_search():
             #         label=bike["title"],
             #         images=bike["images"],
             #     )
-
-
-start_searching = st.button(
-    label="Start Searching",
-)
-if start_searching:
-    execute_search()
